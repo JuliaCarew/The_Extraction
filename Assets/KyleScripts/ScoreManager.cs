e69a06b361998d217f1162ec817ce4e6fc995836
kylelirette93 <162719550+kylelirette93@users.noreply.github.com>
2026-01-08 12:08:29 -0400

    Added score manager.
    Implemented Ben and my logic to score manager.

--- Assets/KyleScripts/ScoreManager.cs
+++ Assets/KyleScripts/ScoreManager.cs
@@ -3,9 +3,24 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     private int teethCollected = 0;
-    private float roomClearedTime = 0f;
+    private int roomClearedTime = 0;
     private int moneyCollected = 0;
-    private float stealthScore = 0f;
+    private int stealthScore = 0;
+
+    private int totalScore = 0;
+
+    private void Awake()
+    {
+        // Subscribe to room cleared event to record time.
+        // Subscribe to enemy died event, to increase stealth score.
+        // Subscribe to tooth collected event.
+        // Subscribe to money collected event.
+    }
+
+    private void OnDestroy()
+    {
+        // Unsubscribe from stuff here.
+    }
 
     private void CollectTooth()
     {
@@ -15,5 +30,25 @@ public class ScoreManager : MonoBehaviour
     private void CollectMoney(int value)
     {
         moneyCollected += value;
+    }
+
+    private void EnemyDied(float stealthBonus)
+    {
+        stealthScore += Mathf.RoundToInt(stealthBonus);
+    }
+
+    private void RoomCleared(float timeTaken)
+    {
+        roomClearedTime = Mathf.RoundToInt(timeTaken);
+    }
+
+    private int CalculateTotalScore()
+    {
+        return teethCollected + moneyCollected + stealthScore + roomClearedTime;
+    }
+
+    public int GetTotalScore()
+    {
+        return CalculateTotalScore();
     }
 }
