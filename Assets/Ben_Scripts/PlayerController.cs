cf43bc0fa2cd6a8a3d44e732b1d27eb728f751cc
BenjaminLM95 <benjamin.lerner95@gmail.com>
2026-01-09 14:43:16 -0400

    Fix the player facing foward issue

--- Assets/Ben_Scripts/PlayerController.cs
+++ Assets/Ben_Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,7 +6,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private InputManager inputManager;
 
     public float movSpeed = 4f;
-    public Rigidbody rb; 
+    public Rigidbody rb;
+
+
+    // Rotation variables
+    [Header("Rotation")]
+    public float rotSpeed = 0.1f;
+    public float turnSmoothTime = 0.1f;
 
     // Input Variables
 
@@ -25,26 +31,40 @@ public class PlayerController : MonoBehaviour
     }
 
     private void FixedUpdate()
-    {
+    {       
         ApplyMovement(); 
     }
 
     public void ApplyMovement() 
     {
-        //Get input direction
-        Vector3 moveInputDirection = new Vector3(moveInput.x, 0, moveInput.y);
-        Vector3 worldMoveDirection = transform.TransformDirection(moveInputDirection);
+        Vector3 moveDirection = new Vector3(moveInput.x, 0f, moveInput.y);
 
-        rb.linearVelocity = worldMoveDirection * movSpeed;
+        // No input → no movement or rotation
+        if (moveDirection.sqrMagnitude < 0.01f)
+        {
+            rb.linearVelocity = Vector3.zero;
+            return;
+        }
 
+        moveDirection.Normalize();
 
+        // Move
+        rb.linearVelocity = moveDirection * movSpeed;
+
+        // Rotate to face movement direction
+        Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+        rb.MoveRotation(Quaternion.Slerp(
+            rb.rotation,
+            targetRotation,
+            rotSpeed
+        ));
     }
 
     void SetMoveInput(Vector2 inputVector)
     {
         moveInput = new Vector2(inputVector.x, inputVector.y);
     }
-
+     
     private void OnEnable()
     {
         inputManager.MoveInputEvent += SetMoveInput; 
